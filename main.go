e66315e4e8b20ea60a3bf8e778ea03c205557be2
tomkalva <bvanflez@gmail.com>
2026-07-02 13:46:29 +0300

    add fileserver hit counter with metrics and reset endpoints

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+
+	apiCfg := apiConfig{}
 
 	mux := http.NewServeMux()
 
@@ -17,8 +32,17 @@ func main() {
 		w.Write([]byte("OK"))
 	})
 
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(fmt.Sprintf("Hits: %d", apiCfg.fileserverHits.Load())))
+	})
+
+	mux.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
+		apiCfg.fileserverHits.Store(0)
+		w.Write([]byte("OK"))
+	})
+
 	handler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
-	mux.Handle("/app/", handler)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
 
 	srv := &http.Server{
 		Addr:    ":" + port,
